9859e63693a55be36dc0850e7714a0424351a9b7
IvanGonzalez05 <igonzalezhansen@gmail.com>
2023-07-26 12:22:14 -0400

    Create function to retrieve all dealerships in index-async-await

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -5,17 +5,33 @@
 const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
+// async function main(params) {
+//       const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+//       const cloudant = CloudantV1.newInstance({
+//           authenticator: authenticator
+//       });
+//       cloudant.setServiceUrl(params.COUCH_URL);
+
+//       try {
+//         let dbList = await cloudant.getAllDbs();
+//         return { "dbs": dbList.result };
+//       } catch (error) {
+//           console.log(error)
+//           return { error: error.description };
+//       }
+// }
+
 async function main(params) {
-      const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
       const cloudant = CloudantV1.newInstance({
           authenticator: authenticator
       });
       cloudant.setServiceUrl(params.COUCH_URL);
+
       try {
-        let dbList = await cloudant.getAllDbs();
-        return { "dbs": dbList.result };
+          const dealerships = await cloudant.postAllDocs({ db: 'dealerships', includeDocs: true });
+          return JSON.stringify({ body: dealerships.result.rows });
       } catch (error) {
-          return { error: error.description };
+          return { error: error.result };
       }
 }
-
