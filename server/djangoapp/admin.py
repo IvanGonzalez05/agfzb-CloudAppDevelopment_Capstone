dde6c75baa31eb0c9f33083b020b09ea0b4437f5
IvanGonzalez05 <igonzalezhansen@gmail.com>
2023-07-27 09:14:11 -0400

    Add CarModel and CarMake models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    inlines= [CarModelInline]
 
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+admin.site.register(CarModel)
+admin.site.register(CarMake)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,22 +4,23 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=50, null=False, primary_key=True)
+    description = models.CharField(max_length=100, null=False)
 
+    def __str__(self):
+        return self.name
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.CharField(max_length=50, null=False, primary_key=True)
+    dealer_id = models.IntegerField(null=False)
+    type = models.CharField(max_length=50)
+    year = models.DateField(null=True)
+    # many to many relationship with car make
+    carmakes = models.ManyToManyField(CarMake)
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
